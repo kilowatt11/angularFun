0e0bf55aeaf9ad5d9e7c111fb3ff6e0a2b55d8f2
kilowatt11 <kilowatt11@cableone.net>
2016-03-03 22:49:35 -0700

    an in class angular challenge

--- app/controller.js
+++ app/controller.js
@@ -1,8 +1,24 @@
-app.controller('buttonController', function ($scope){
-      $scope.clicks = 0;
-    $scope.click = function (){     
-       $scope.clicks += 1;
+app.controller('buttonController', function ($scope, $timeout) {
+    $scope.clicks = 35;
+    $scope.timer = function () {
+        $scope.clicks--;
+        $scope.timeout = $timeout($scope.timer, 1000)
+       
+       if($scope.clicks == 0){
+           $scope.stop();
+           
+       }
+       
+       
+       
              
+    };
+
+    $scope.start = function () {
+        $scope.timer();
+    };
+    $scope.stop = function () {
+        $timeout.cancel($scope.timeout);
     }
-  $scope.leapYear = function(){
-      
+
+});
